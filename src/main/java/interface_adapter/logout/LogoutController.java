f55a083cb2c13aeb8f72c3a28e0813075ecff05d
Daniel Benavides-Alvarez <daniel.benavidesalvarez@mail.utoronto.ca>
2024-10-21 19:56:23 -0400

    LogoutController and LogoutOutputData TODOs

--- src/main/java/interface_adapter/logout/LogoutController.java
+++ src/main/java/interface_adapter/logout/LogoutController.java
@@ -1,6 +1,8 @@
 package interface_adapter.logout;
 
+import use_case.login.LoginInputData;
 import use_case.logout.LogoutInputBoundary;
+import use_case.logout.LogoutInputData;
 
 /**
  * The controller for the Logout Use Case.
@@ -10,7 +12,7 @@ public class LogoutController {
     private LogoutInputBoundary logoutUseCaseInteractor;
 
     public LogoutController(LogoutInputBoundary logoutUseCaseInteractor) {
-        // TODO: Save the interactor in the instance variable.
+        this.logoutUseCaseInteractor = logoutUseCaseInteractor;
     }
 
     /**
@@ -18,8 +20,9 @@ public class LogoutController {
      * @param username the username of the user logging in
      */
     public void execute(String username) {
-        // TODO: run the use case interactor for the logout use case
         // 1. instantiate the `LogoutInputData`, which should contain the username.
         // 2. tell the Interactor to execute.
+        final LogoutInputData logoutInputData = new LogoutInputData(username);
+        logoutUseCaseInteractor.execute(logoutInputData);
     }
 }
