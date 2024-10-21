d8971ab41a79a93d215d4a22667566714ff2790c
luiscc02 <luis.calderon@mail.utoronto.ca>
2024-10-21 19:57:10 -0400

    DannyInputData

--- src/main/java/use_case/logout/LogoutInputData.java
+++ src/main/java/use_case/logout/LogoutInputData.java
@@ -4,9 +4,28 @@ package use_case.logout;
  * The Input Data for the Logout Use Case.
  */
 public class LogoutInputData {
+    private final String username;
 
     public LogoutInputData(String username) {
-        // TODO: save the current username in an instance variable and add a getter.
+        // Save the current username in an instance variable.
+        this.username = username;
     }
 
+    // Getter method for the username.
+    public String getUsername() {
+        return this.username;
+    }
 }
+
+//package use_case.logout;
+//
+///**
+// * The Input Data for the Logout Use Case.
+// */
+//public class LogoutInputData {
+//
+//    public LogoutInputData(String username) {
+//        // TODO: save the current username in an instance variable and add a getter.
+//    }
+//
+//}
